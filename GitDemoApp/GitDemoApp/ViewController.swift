dc4641475810308cd32552e6d75937cb7e72cfb0
Sachith Rao <smohanra@teksystems.com>
2017-05-18 11:52:39 +0530

    Sample demo app created

---
+++ GitDemoApp/GitDemoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemoApp
+//
+//  Created by Sachith Rao on 18/05/17.
+//  Copyright © 2017 TEKSystems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
